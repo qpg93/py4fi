e42f78d45a469a70c0eb682aab5d16e4faf31efd
qpg93 <qingp.peng@outlook.fr>
2022-01-11 00:02:43 +0800

    basic data types

--- ch03/data_type_struct.py
+++ ch03/data_type_struct.py
@@ -1,3 +1,9 @@
+""" data types and structures """
+import decimal
+from decimal import Decimal
+import re
+from datetime import datetime
+
 # data types
 A = 10
 print('type of A:', type(A))
@@ -19,9 +25,6 @@ C = 0.5
 print('C as integer ratio:', C.as_integer_ratio())
 print('B as integer ratio:', B.as_integer_ratio())
 
-import decimal
-from decimal import Decimal
-
 print('Decimal context:', decimal.getcontext())
 D = Decimal(1) / Decimal(11)
 print('D:', D)
@@ -37,3 +40,46 @@ print('F:', F)
 G = D + E + F
 print('G:', G)
 
+print('int(True):', int(True))
+print('int(False):', int(False))
+print('float(True):', float(True))
+print('float(False):', float(False))
+print('bool(0):', bool(0))
+print('bool(0.0):', bool(0.0))
+print('bool(1):', bool(1))
+print('bool(10.5):', bool(10.5))
+print('bool(-2):', bool(-2))
+
+T = 'this is a string object'
+print('T capitalize:', T.capitalize())
+print('T split:', T.split())
+print('T find string:', T.find('string'))
+print('T find python:', T.find('Python'))
+print('T replace:', T.replace(' ', '|'))
+
+URL = 'http://www.python.org'
+print('URL:', URL.strip('htp:/'))
+
+print(f'this is an integer {15:d}')
+print(f'this is an integer {15:4d}')
+print(f'this is an integer {15:04d}')
+print(f'this is an integer {15.3456:f}')
+print(f'this is an integer {15.3456:.2f}')
+print(f'this is an integer {15.3456:8f}')
+print(f'this is an integer {15.3456:8.2f}')
+print(f'this is an integer {15.3456:08.2f}')
+print(f'this is an integer {"Python":s}.')
+print(f'this is an integer {"Python":10s}.')
+
+SERIES = """
+'01/18/2014 13:00:00', 100, '1st';
+'01/18/2014 13:30:00', 110, '2nd';
+'01/18/2014 14:00:00', 120, '3rd';
+"""
+DT = re.compile(r"'[0-9/:\s]+'")
+result = DT.findall(SERIES)
+print('result:', result)
+
+pydt = datetime.strptime(result[0].replace("'", ""),
+                         "%m/%d/%Y %H:%M:%S")
+print(pydt, type(pydt))
