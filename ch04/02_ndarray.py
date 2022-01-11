8474616e224544e1f3304e8fef8d3648981190c9
Qing Peng <qing.peng@outlook.fr>
2022-01-11 09:40:33 +0000

    ndarray creation

--- ch04/02_ndarray.py
+++ ch04/02_ndarray.py
@@ -52,3 +52,43 @@ T2 = timeit(stmt='math.sqrt(2.5)',
             number=1000000)
 print('Timeit np sqrt:', T1)
 print('Timeit math sqrt:', T2)
+
+# multiple dimensions
+B = np.array([A, A * 2])
+pprint(B)
+
+print('B[0]:', B[0])
+print('B[0, 2]:', B[0, 2])
+print('B[:, 1]:', B[0])
+print('B sum:', B.sum())
+print('B sum axis=0:', B.sum(axis=0))
+print('B sum axis=1:', B.sum(axis=1))
+
+C = np.zeros((2, 3), dtype='i', order='C')
+pprint(C)
+
+C = np.ones((2, 3, 4), dtype='i', order='C')
+pprint(C)
+
+D = np.zeros_like(C, dtype='f16', order='C')
+pprint(D)
+
+D = np.ones_like(C, dtype='f16', order='C')
+pprint(D)
+
+# array not prepopulated with anything
+# numbers depend on the bits present in the memory
+E = np.empty((2, 3, 2))
+pprint(E)
+
+F = np.empty_like(C)
+pprint(F)
+
+# diagonal populated by 1
+G = np.eye(5)
+pprint(G)
+
+# one-dimensional array evenly spaced intervals between numbers
+# parameters: start, end, number of elements
+G = np.linspace(5, 15, 12)
+pprint(G)
