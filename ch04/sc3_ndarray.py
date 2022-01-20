f71e921a71d3d60205daa47e2a9e56df2a06fb59
qpg93 <qingp.peng@outlook.fr>
2022-01-20 18:59:18 +0800

    structured ndarray & basic vectorization

--- ch04/sc3_ndarray.py
+++ ch04/sc3_ndarray.py
@@ -24,3 +24,45 @@ print("S['Height'] mean:", S['Height'].mean())
 print('S[0]:', S[0])
 # field selection in a record(line)
 print("S[1]['Age']:", S[1]['Age'])
+
+# basic vectorization
+np.random.seed(100)
+R = np.arange(12).reshape((4, 3))
+S = np.arange(12).reshape((4, 3)) * 0.5
+print('R:', R)
+print('S:', S)
+# element-wise addition
+print('R + S:', R + S)
+# scalar is broadcast and added to every element
+print('R + 3:', R + 3)
+# scalar is broadcast and multiplied to every element
+print('2 * R:', 2 * R)
+print('2 * R + 3:', 2 * R + 3)
+
+print('R shape:', R.shape)
+S = np.arange(0, 12, 4)
+print('S:', S)
+print('R + S:', R + S)
+
+S = np.arange(0, 12, 3)
+print('S:', S)
+# ValueError: operands could not be broadcast together with different shapes
+# print('R + S:', R + S)
+
+# transposing R again allows for vectorized addition
+print('R.transpose() + S:', R.transpose() + S)
+
+SR = S.reshape(-1, 1)
+print('SR:', SR)
+print('SR shape:', SR.shape)
+print('R + S.reshape:', R + S.reshape(-1, 1))
+
+
+def calc(num):
+    """ calculate y=3x+5 """
+    return 3 * num + 5
+
+
+print('calc(0.5):', calc(0.5))
+# function applied to ndarray object (vectorized & element-wise)
+print('calc(R):', calc(R))
