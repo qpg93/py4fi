fab20dbf26784f45daa505e68468ed6e73e2b478
qpg93 <qingp.peng@outlook.fr>
2022-02-16 19:59:35 +0800

    update output syntax

--- ch05/sc1_dataframe.py
+++ ch05/sc1_dataframe.py
@@ -101,14 +101,14 @@ df['Quarter'] = ['Q1', 'Q1', 'Q1', 'Q2', 'Q2', 'Q2', 'Q3', 'Q3', 'Q3']
 print('df:', df)
 groups = df.groupby('Quarter')
 print('groups size: ', groups.size())
-print('gouprs mean', groups.mean())
-print('gouprs max', groups.max())
+print('gouprs mean:', groups.mean())
+print('gouprs max:', groups.max())
 # gives both the minimum and maximum values per column
-print('gouprs aggregate', groups.aggregate([min, max]).round(2))
+print('gouprs aggregate:', groups.aggregate([min, max]).round(2))
 
 df['Odd_Even'] = ['Odd', 'Even', 'Odd', 'Even', 'Odd', 'Even',
                   'Odd', 'Even', 'Odd']
 groups = df.groupby(['Quarter', 'Odd_Even'])
 print('groups size: ', groups.size())
 
-print('groups aggregate', groups[['No1', 'No4']].aggregate([sum, np.mean]))
+print('groups aggregate:', groups[['No1', 'No4']].aggregate([sum, np.mean]))
